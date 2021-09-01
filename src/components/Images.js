7106f384968079906465b7d27f41ddfcebcc3f92
JackCS <jackfcs22@gmail.com>
2021-09-01 16:43:59 +0100

    Project images imported and objects created

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,15 +1,74 @@
-import bootstrap from "../icons/frontend/bootstrap.png"
-import css3 from "../icons/frontend/css3.png"
-import html5 from "../icons/frontend/html5.png"
-import javascript from "../icons/frontend/javascript.png"
-import react from "../icons/frontend/react.png"
-import sass from "../icons/frontend/sass.png"
-import git from "../icons/other/git.png"
-import github from "../icons/other/github.png"
-import jest from "../icons/other/jest.png"
-import npm from "../icons/other/npm.png"
-import webpack from "../icons/other/webpack.png"
+import bootstrap from "../icons/frontend/bootstrap.png";
+import css3 from "../icons/frontend/css3.png";
+import html5 from "../icons/frontend/html5.png";
+import javascript from "../icons/frontend/javascript.png";
+import react from "../icons/frontend/react.png";
+import sass from "../icons/frontend/sass.png";
+import git from "../icons/other/git.png";
+import github from "../icons/other/github.png";
+import jest from "../icons/other/jest.png";
+import npm from "../icons/other/npm.png";
+import webpack from "../icons/other/webpack.png";
+import battleship from "../icons/screenshots/battleship.png";
+import shoppingCart from "../icons/screenshots/shopping-cart.png";
+import cv from "../icons/screenshots/cv.png";
+import weather from "../icons/screenshots/weather.png";
+import website from "../icons/screenshots/website.png";
+import todo from "../icons/screenshots/todo.png";
+import calculator from "../icons/screenshots/calculator.png";
+import library from "../icons/screenshots/library.png";
 
 export const frontEnd = [javascript, html5, css3, react, bootstrap, sass];
 
-export const misc =[git, github, npm, webpack, jest]
+export const misc = [git, github, npm, webpack, jest];
+
+export const screenShots = [
+  {
+    name: "Battleship",
+    image: battleship,
+    repo: "https://github.com/Jackfcs/battleship",
+    live: "https://jackfcs.github.io/battleship/",
+  },
+  {
+    name: "Shopping Cart",
+    image: shoppingCart,
+    repo: "https://github.com/Jackfcs/shopping-cart",
+    live: "https://jackfcs.github.io/shopping-cart/",
+  },
+  {
+    name: "CV Generator",
+    image: cv,
+    repo: "https://github.com/Jackfcs/cv-project",
+    live: "https://jackfcs.github.io/cv-project/",
+  },
+  {
+    name: "Weather App",
+    image: weather,
+    repo: "https://github.com/Jackfcs/weather-forecast-app",
+    live: "https://jackfcs.github.io/weather-forecast-app/",
+  },
+  {
+    name: "Personal Website",
+    image: website,
+    repo: "https://github.com/Jackfcs/personal-website",
+    live: "https://google.com",
+  },
+  {
+    name: "Todo App",
+    image: todo,
+    repo: "https://github.com/Jackfcs/to-do-library",
+    live: "https://jackfcs.github.io/to-do-library/",
+  },
+  {
+    name: "Calculator",
+    image: calculator,
+    repo: "https://github.com/Jackfcs/calculator",
+    live: "https://jackfcs.github.io/calculator/",
+  },
+  {
+    name: "Book Library",
+    image: library,
+    repo: "https://github.com/Jackfcs/book_library",
+    live: "https://jackfcs.github.io/book_library/",
+  }
+];
