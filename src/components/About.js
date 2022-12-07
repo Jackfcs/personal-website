6b9fd56b13d0eaffc8430f7998261a02681f2590
Jack Campbell-Smith <jackfcs22@gmail.com>
2022-12-07 15:03:22 +0000

    Update CV and about text

--- src/components/About.js
+++ src/components/About.js
@@ -46,33 +46,11 @@ const About = () => {
     <Container id="about">
       <SectionTitle>About me</SectionTitle>
       <SectionText>
-        I am a results driven and problem solving programmer looking to advance my
-        career in software development. During a successful career in content
-        delivery and management, I discovered my passion for programming when
-        using Python to create a script that automated a previously
-        time-intensive task for my team. I found the process of learning to code
-        and then writing and executing the script to be both hugely rewarding and satisfying.
-        <br />
-        <br />
-        Something clicked.
-        <br />
-        <br />
-        From this point on I realised programming was something that I wanted to
-        pursue. For over a year I have been developing my technical abilities
-        and knowledge at every opportunity outside of my working hours. I have
-        been meticulously following an online curriculum called The Odin
-        Project, which provides both the resources and community in order to
-        become a highly skilled developer.
-        <br />
-        <br />
-        It has been an extremely enjoyable process; I relish the tough
-        challenges caused by a problem, followed by the high that comes from
-        solving them and the fact that there will always be something new to
-        learn.
-        <br />
-        <br />
-        Please get in touch if you have any questions or if you’d like to
-        collaborate.
+        I am a highly motivated and diligent JavaScript developer, with a focus
+        on Front End. I'm passionate about finding the right solution to the
+        problem and focused on continually learning and developing my existing
+        skills and expertise to enable this. I'm a team player, work well under
+        pressure and enjoy projects that challenge me.
       </SectionText>
     </Container>
   );
